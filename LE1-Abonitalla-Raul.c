39976762f173813fb707fed9969c261c1231e80a
abonitallaraul07 <abonitalla.raul07@gmail.com>
2026-08-16 11:59:40 +0800

    Add files via upload

---
+++ LE1-Abonitalla-Raul.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+
+
+    
+#include <stdio.h>
+
+int main() {
+    printf("======================================\n");
+    printf("    STUDENT PROFILE\n");
+    printf("======================================\n");
+    printf("Name: Raul C. Abonitalla\n");
+    printf("Course and Section: BSIT-1R14\n");
+    printf("Year Level: 1st Year\n");
+    printf("======================================\n");
+    printf("    FUTURE PROGRAMMER\n");
+    printf("======================================\n");
+
+    return 0;
+}
+    
+
